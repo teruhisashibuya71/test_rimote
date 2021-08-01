244d324ddff6eefb53591029b8b778e01a8dee09
teruhisashibuya <lshibuya.tk@gmail.com>
2021-08-01 19:23:12 +0900

    コミット1-yusha.rb作成と記述

---
+++ yusha.rb
@@ -0,0 +1,10 @@
+require './monster'
+
+class Yusha
+   def initialize(name, hp, offense, defense)
+       @name = name
+       @hp = hp
+       @offense = offense
+       @defense = defense
+   end
+end
